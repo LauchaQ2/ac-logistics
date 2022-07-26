dcef2fab5cc6e290b5285d6a786366da14225461
Lautaro Quevedo <89219479+LauchaQ2@users.noreply.github.com>
2022-07-25 22:47:03 -0300

    EmailJs added

--- script.js
+++ script.js
@@ -35,3 +35,24 @@ function numbersOnly(input){
   var regex = /[^0-9]/g;
   input.value = input.value.replace(regex, "");
 }
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'ENVIANDO';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_8c27fij';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('CONSULTA ENVIADA!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
